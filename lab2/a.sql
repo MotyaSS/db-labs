f4ae96897dc12d0d5e6a624a786f7bf5a30bd0ba
motya <motayzz32@gmail.com>
2024-10-15 20:40:32 +0300

    lab2

---
+++ lab2/a.sql
@@ -0,0 +1,54 @@
+SELECT *
+FROM vehicle ORDER BY hour_rent_price;
+SELECT *
+FROM client ORDER BY id;
+
+CREATE TABLE IF NOT EXISTS vehicle (
+    id              SERIAL PRIMARY KEY,
+    type            VARCHAR(255) NOT NULL,
+    brand           VARCHAR(255) NOT NULL,
+    model           VARCHAR(255) NOT NULL,
+    color           CHAR(7)      NOT NULL,
+    hour_rent_price MONEY        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS client (
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    phone      CHAR(11)     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS payment_methods (
+    id     SERIAL PRIMARY KEY,
+    method VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS rent (
+    id               SERIAL PRIMARY KEY,
+    vehicle_id       INT       NOT NULL,
+    client_id        INT       NOT NULL,
+    affirmation_date TIMESTAMP NOT NULL,
+    start_date       TIMESTAMP NOT NULL,
+    end_date         TIMESTAMP NOT NULL,
+    total_cost       MONEY     NOT NULL,
+    payment_method   INT       NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle (id),
+    FOREIGN KEY (client_id) REFERENCES client (id)
+);
+
+INSERT INTO vehicle (type, brand, model, color, hour_rent_price)
+VALUES ('Truck', 'Ford', 'F-150', '#00FF00', 30.00),
+       ('Truck', 'Chevrolet', 'Silverado', '#FFA500', 28.00),
+       ('Bike', 'Yamaha', 'YZF-R3', '#FF0000', 15.00),
+       ('Bike', 'Kawasaki', 'Ninja 400', '#008000', 18.00),
+       ('Car', 'Ford', 'Mustang', '#0000FF', 25.00),
+       ('Car', 'Lamborghini', 'Aventador', '#FFFF00', 50.00);
+
+INSERT INTO client (first_name, last_name, email, phone)
+VALUES ('John', 'Doe', 'john.doe@example.com', '12345678901'),
+       ('Jane', 'Smith', 'jane.smith@example.com', '10987654321'),
+       ('Alice', 'Johnson', 'alice.johnson@example.com', '11223344556'),
+       ('Bob', 'Brown', 'bob.brown@example.com', '66554433221'),
+       ('Kirill', 'Vasilev', 'kir_vas@example.com', '22010029318');
---
+++ lab2/queries.sql
@@ -0,0 +1,109 @@
+-- 3
+SELECT *
+FROM vehicle
+ORDER BY hour_rent_price;
+
+-- 4
+SELECT client_id
+FROM rent
+WHERE
+    (start_date >= '2023-01-22 00:00:00' AND end_date <= '2023-01-30 23:59:59')
+GROUP BY client_id ORDER BY client_id;
+-- 4 alt
+SELECT *
+FROM client
+WHERE
+    id IN
+    (SELECT rent.client_id FROM rent WHERE (start_date >= '2023-01-22 00:00:00' AND end_date <= '2023-01-30 23:59:59'));
+
+-- 5
+SELECT *
+FROM vehicle
+WHERE
+      hour_rent_price >= 20::MONEY
+  AND hour_rent_price < 30::MONEY;
+
+-- 6
+SELECT client_id, SUM(total_cost) AS total_rent_cost
+FROM rent
+client_id
+GROUP BY client_id
+ORDER BY client_id;
+
+SELECT *
+FROM client;
+
+-- 7
+-- Никак без JOIN
+
+-- 8
+SELECT client_id, SUM(total_cost) AS total_rent_cost
+FROM rent
+GROUP BY client_id
+HAVING
+    SUM(total_cost) > 1300::MONEY;
+
+
+-- 9
+SELECT *
+FROM vehicle;
+UPDATE vehicle
+SET hour_rent_price = 22::MONEY
+WHERE
+    type = 'Bike';
+
+UPDATE vehicle
+SET hour_rent_price = 15::MONEY
+WHERE
+    brand = 'Yamaha';
+UPDATE vehicle
+SET hour_rent_price = 18::MONEY
+WHERE
+    brand = 'Kawasaki';
+
+-- 10
+DELETE
+FROM rent
+WHERE
+    affirmation_date < CURRENT_DATE - INTERVAL '1 year';
+
+-- 11
+SELECT *
+FROM rent
+WHERE
+    vehicle_id IN (SELECT id FROM vehicle WHERE type = 'Car');
+
+-- 12
+SELECT *
+FROM rent
+LIMIT 5 OFFSET 23;
+
+-- 13
+SELECT type, AVG(hour_rent_price) AS average_rent_price
+FROM vehicle
+GROUP BY type;
+
+-- 14
+SELECT *
+FROM rent
+ORDER BY affirmation_date;
+
+-- 15
+SELECT *
+FROM client
+WHERE id IN (
+    SELECT client_id
+    FROM rent
+    GROUP BY client_id
+    ORDER BY COUNT(id) DESC
+);
+-- to check 15
+SELECT
+    r.client_id,
+    COUNT(r.id) AS rental_count
+FROM
+    rent r
+GROUP BY
+    r.client_id
+ORDER BY
+    rental_count DESC;
